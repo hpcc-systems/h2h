31ffb5fd01ca5db6ee11c63ae0d653deb1e660b8
Rodrigo Pastrana <Rodrigo.Pastrana@lexisnexis.com>
2012-07-11 17:31:24 -0400

    gh-1 Initial check-in into h2h dedicated repo
    Code moves from HPCC-Platform/plugins/dataconnectors/hdfsconnector to h2h
    
    Signed-off-by: Rodrigo Pastrana <Rodrigo.Pastrana@lexisnexis.com>

---
+++ version.cmake
@@ -0,0 +1,10 @@
+###
+## Version Information
+###
+set ( HPCC_PROJECT "community" )
+set ( HPCC_MAJOR 3 )
+set ( HPCC_MINOR 6 )
+set ( HPCC_POINT 4 )
+set ( HPCC_MATURITY "beta" )
+set ( HPCC_SEQUENCE 1 )
+###
