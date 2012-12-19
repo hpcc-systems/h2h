7402428b5f8d3e9aabf8c198986b03fa6ac835cb
Rodrigo Pastrana <Rodrigo.Pastrana@lexisnexis.com>
2012-12-19 10:45:53 -0500

    HH-55 1.0.0.rc1 Signed-off-by: Rodrigo Pastrana <Rodrigo.Pastrana@lexisnexis.com>:q!

--- version.cmake
+++ version.cmake
@@ -1,9 +1,9 @@
 ###
 ## Version Information
 ###
-set ( HPCC_MAJOR 4 )
+set ( HPCC_MAJOR 1 )
 set ( HPCC_MINOR 0 )
 set ( HPCC_POINT 0 )
-set ( HPCC_MATURITY "beta" )
+set ( HPCC_MATURITY "rc" )
 set ( HPCC_SEQUENCE 1 )
 ###
