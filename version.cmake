ebcbf0439059f79eaa21528b1c69cf2dc5d24adb
Rodrigo Pastrana <Rodrigo.Pastrana@lexisnexis.com>
2013-01-16 11:39:53 -0500

    HH-59 Set version to 1.4.0-1 Version reset, going away from version inherited from HPCC Platform beta 3.x
    Signed-off-by: Rodrigo Pastrana <Rodrigo.Pastrana@lexisnexis.com>

--- version.cmake
+++ version.cmake
@@ -2,8 +2,8 @@
 ## Version Information
 ###
 set ( HPCC_MAJOR 1 )
-set ( HPCC_MINOR 0 )
+set ( HPCC_MINOR 4 )
 set ( HPCC_POINT 0 )
-set ( HPCC_MATURITY "rc" )
+set ( HPCC_MATURITY "release" )
 set ( HPCC_SEQUENCE 1 )
 ###
