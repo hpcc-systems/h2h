2482cb95c4c3bd893c542163794b82d26b8d561b
Rodrigo Pastrana <Rodrigo.Pastrana@lexisnexis.com>
2013-11-05 14:29:11 -0500

    HH-67 add raring support - add distro deps - ensure proper config install - expand logging statements
    Signed-off-by: Rodrigo Pastrana <Rodrigo.Pastrana@lexisnexis.com>

--- cmake_modules/dependencies/quantal.cmake
+++ cmake_modules/dependencies/quantal.cmake
@@ -1,4 +1,4 @@
-#ubuntu 12.04 precise
+#Ubuntu 12.10 Quantal
 IF ( BUILD_WEBHDFS_VER )
 	set ( CPACK_DEBIAN_PACKAGE_DEPENDS "hpccsystems-platform, libcurl3, libcurl3-nss")
 ELSE ()
--- cmake_modules/dependencies/saucy.cmake
+++ cmake_modules/dependencies/saucy.cmake
@@ -1,4 +1,4 @@
-#ubuntu 12.04 precise
+#Ubuntu 13.10
 IF ( BUILD_WEBHDFS_VER )
 	set ( CPACK_DEBIAN_PACKAGE_DEPENDS "hpccsystems-platform, libcurl3, libcurl3-nss")
 ELSE ()
