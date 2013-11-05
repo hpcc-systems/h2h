187ce6339c58ddc8c4f31b8076cf2af3f495bb41
Rodrigo Pastrana <Rodrigo.Pastrana@lexisnexis.com>
2013-11-05 17:20:18 -0500

    HH-67 -- no hadoop standard install for 2.2
    Signed-off-by: Rodrigo Pastrana <Rodrigo.Pastrana@lexisnexis.com>

--- cmake_modules/dependencies/precise.cmake
+++ cmake_modules/dependencies/precise.cmake
@@ -2,5 +2,6 @@
 IF ( BUILD_WEBHDFS_VER )
 	set ( CPACK_DEBIAN_PACKAGE_DEPENDS "hpccsystems-platform, libcurl3, libcurl3-nss")
 ELSE ()
-    set ( CPACK_DEBIAN_PACKAGE_DEPENDS "hpccsystems-platform, hadoop, openjdk-6-jre")
+    #set ( CPACK_DEBIAN_PACKAGE_DEPENDS "hpccsystems-platform, hadoop, openjdk-6-jre")
+    set ( CPACK_DEBIAN_PACKAGE_DEPENDS "hpccsystems-platform, openjdk-6-jre")
 ENDIF ()
