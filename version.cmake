1bedf17cdd0f5eb94354fc59538955d13e1952a6
rpastrana <rodrigo.pastrana@lexisnexis.com>
2014-03-18 14:50:02 -0400

    INTERNAL version update
    Signed-off-by: rpastrana <rodrigo.pastrana@lexisnexis.com>

--- version.cmake
+++ version.cmake
@@ -2,8 +2,8 @@
 ## Version Information
 ###
 set ( HPCC_MAJOR 1 )
-set ( HPCC_MINOR 4 )
-set ( HPCC_POINT 2 )
-set ( HPCC_MATURITY "release" )
+set ( HPCC_MINOR 6 )
+set ( HPCC_POINT 0 )
+set ( HPCC_MATURITY "trunk" )
 set ( HPCC_SEQUENCE 1 )
 ###
