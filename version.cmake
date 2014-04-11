5cc4053213b662e904d2047d373035c2ced26cac
rpastrana <rodrigo.pastrana@lexisnexis.com>
2014-04-11 13:57:03 -0400

    #INTERNAL update version Signed-off-by: rpastrana <rodrigo.pastrana@lexisnexis.com>

--- version.cmake
+++ version.cmake
@@ -3,7 +3,7 @@
 ###
 set ( HPCC_MAJOR 1 )
 set ( HPCC_MINOR 4 )
-set ( HPCC_POINT 2 )
-set ( HPCC_MATURITY "release" )
+set ( HPCC_POINT 4 )
+set ( HPCC_MATURITY "trunk" )
 set ( HPCC_SEQUENCE 1 )
 ###
