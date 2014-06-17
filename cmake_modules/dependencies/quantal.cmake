e3161c2fa3a3d751d73dfcd8beb75428fc3f8fe0
rpastrana <rodrigo.pastrana@lexisnexis.com>
2014-06-17 10:19:58 -0400

    HH-98 Add Ubuntu 1404 support
    Signed-off-by: rpastrana <rodrigo.pastrana@lexisnexis.com>

--- cmake_modules/dependencies/quantal.cmake
+++ cmake_modules/dependencies/quantal.cmake
@@ -14,7 +14,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
-
+MESSAGE("--- building Ubuntu 12.10 which is no longer supported --")
 #Ubuntu 12.10 Quantal
 IF ( BUILD_WEBHDFS_VER )
 	set ( CPACK_DEBIAN_PACKAGE_DEPENDS "hpccsystems-platform, libcurl3, libcurl3-nss")
